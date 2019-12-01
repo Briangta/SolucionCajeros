83cd18fc43fa5d878bd1ed29cc189930c87e3863
Arthur Brian Gil Paredes <arthurbrian2@gmail.com>
2019-11-30 22:55:20 -0500

    agregar un evento

--- Cajeros.Web/Controllers/RutasController.cs
+++ Cajeros.Web/Controllers/RutasController.cs
@@ -22,5 +22,21 @@ namespace Cajeros.Web.Controllers
             return View();
         }
 
+        public ActionResult AgregarUnEvento()
+        {
+            List<SelectListItem> items = new List<SelectListItem> { new SelectListItem { Text = "Todas", Value = "1" },
+                new SelectListItem { Text = "Occidente", Value = "1" },
+                new SelectListItem { Text = "Norte", Value = "1" },
+                new SelectListItem { Text = "Sur", Value = "1" },
+                new SelectListItem { Text = "Centro", Value = "1" } };
+            ViewBag.Zona = new SelectList(items, "Value", "Text");
+
+            List<SelectListItem> itemsTipo = new List<SelectListItem> { new SelectListItem { Text = "Fuera de Servicio", Value = "1" },
+                new SelectListItem { Text = "Aumento", Value = "1" } };
+
+            ViewBag.Zona = new SelectList(items, "Value", "Text");
+            ViewBag.Tipo = new SelectList(itemsTipo, "Value", "Text");
+            return View();
+        }
     }
 }
---
+++ Cajeros.Web/Scripts/site.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    $(".iframe").click(function (e) {
+        e.preventDefault();
+        var ruta = $(this).attr("href");
+        abrirIframe(ruta);
+    });
+});
+
+function abrirIframe(ruta) {
+    $.fancybox.open({ href: ruta, autoDimensions: true, type: 'iframe' });
+}
